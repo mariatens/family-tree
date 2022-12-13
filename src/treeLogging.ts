e669939f81612b7c2de6d997de87271e5de0c651
Maria <mtens17@gmail.com>
2022-12-13 11:01:38 +0000

    Stack for stark family is working

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,9 +1,27 @@
+import { createStarkTree } from "./families/stark";
 import { Person } from "./personTypes";
 
 
 export function logAllPeopleInTree(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
+    const workStack: Person[]= []
+    workStack.push(topPerson)
+    while(workStack.length >= 1){
+        let currentPerson = workStack.pop() 
+        if (currentPerson === undefined){
+            throw new Error("Unexpectedly got undefined from pop")
+        }
+        console.log(currentPerson.name, "parent")
+        for (let item of currentPerson.children){
+            workStack.push(item)
+            console.log(workStack, "children")
+        } 
+    }
+    console.log("done")
 }
+
+console.log(logAllPeopleInTree(createStarkTree()))
+
+
 
 export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
     console.log("TODO!  First person is " + topPerson.name);
